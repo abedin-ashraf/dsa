256f3349fcef6cad717c09822e4ab9139f1776e6
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-08-28 11:02:26 -0500

    08/28

---
+++ Strings/Strings.java
@@ -0,0 +1,18 @@
+
+package Strings;
+
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[] = { 'a', 'b', 'c', 'd' };
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        // Strings are Immutable
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+
+    }
+}
