03753f147c85c3941fdabe7a4200a63385663c2f
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-09-04 20:19:57 -0500

    Added fibonacci

---
+++ BasicRecursion/NthFibonacciNumber.java
@@ -0,0 +1,17 @@
+package BasicRecursion;
+
+public class NthFibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(nthFIbonacciNumber(10));
+    }
+
+    public static int nthFIbonacciNumber(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return nthFIbonacciNumber(n - 1) + nthFIbonacciNumber(n - 2);
+    }
+}
