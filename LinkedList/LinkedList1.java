0f2324659f78e28a7a4af1658293da1a0139622b
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-09-18 21:59:28 -0500

    Added LinkedList

---
+++ LinkedList/LinkedList1.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedList1 {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+    }
+}
--- leetcode.java
+++ leetcode.java
@@ -1,26 +1,17 @@
 public class leetcode {
     public static void main(String[] args) {
-        int nums[] = { 6, 6, 6, 7, 7 };
-
-        System.out.println(majorityElement(nums));
+        System.out.println(hammingWeight(00000000000000000000000010000000));
     }
 
-    public static int majorityElement(int[] nums) {
-
-        int maxCount = 0;
-        int majorityIndex = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            int count = 1;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+    public static int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) != 0) {
+                count++;
             }
-            if (count > maxCount) {
-                maxCount = count;
-                majorityIndex = i;
-            }
+            n = n >> 1;
         }
-        return nums[majorityIndex];
+        return count;
+
     }
 }
